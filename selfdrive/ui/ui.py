d343cb94780a3f66cbb9e5908482f38690d0e435
Luiz Guilherme Ito <martinho.ito@gmail.com>
2026-02-19 23:20:21 -0300

    fix: pin UI to core 6 on Jetson to avoid locationd collision

--- selfdrive/ui/ui.py
+++ selfdrive/ui/ui.py
@@ -2,7 +2,7 @@
 import os
 import pyray as rl
 
-from openpilot.system.hardware import TICI
+from openpilot.system.hardware import TICI, JETSON
 from openpilot.common.realtime import config_realtime_process, set_core_affinity
 from openpilot.system.ui.lib.application import gui_app
 from openpilot.selfdrive.ui.layouts.main import MainLayout
@@ -12,7 +12,7 @@ from openpilot.common.params import Params
 
 
 def main():
-  cores = {5, }
+  cores = {6} if JETSON else {5}
   config_realtime_process(0, 51)
 
   gui_app.init_window("UI")
@@ -21,13 +21,20 @@ def main():
   else:
     main_layout = MiciMainLayout()
   main_layout.set_rect(rl.Rectangle(0, 0, gui_app.width, gui_app.height))
+
+  if JETSON:
+    try:
+      set_core_affinity(list(cores))
+    except OSError:
+      pass
+
   for should_render in gui_app.render():
     ui_state.update()
     if should_render:
       main_layout.render()
 
       # reaffine after power save offlines our core
-      if TICI and os.sched_getaffinity(0) != cores:
+      if (TICI or JETSON) and os.sched_getaffinity(0) != cores:
         try:
           set_core_affinity(list(cores))
         except OSError:
